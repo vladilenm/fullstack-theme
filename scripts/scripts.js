317f389a9432bd7d89c9b25759ba13b5f485e586
Vadien <mininvlad@gmail.com>
2018-04-22 20:14:42 +0300

    initial

---
+++ scripts/scripts.js
@@ -0,0 +1,39 @@
+const $ = selector => document.querySelector(selector)
+const $$ = selector => document.querySelectorAll(selector)
+
+M.FloatingActionButton.init($('.fixed-action-btn'))
+
+const $tt = $('.tap-target')
+const $modal = $('.modal')
+const $ttp = $('.tooltipped')
+
+if ($tt) {
+  const tapTarget = M.TapTarget.init($tt)
+  $('#dashboard-info').addEventListener('click', function() {
+    tapTarget.open()
+  })
+}
+
+if ($modal) {
+  M.Modal.init($modal)
+}
+
+if ($('.datepicker')) {
+  M.Datepicker.init($$('.datepicker'), {
+    format: 'dd.mm.yyyy'
+  })
+}
+
+const $filter = $('.js-filter')
+
+if ($filter) {
+  $filter.addEventListener('click', () => {
+    $filter.classList.toggle('active')
+    $('.js-filter-block').classList.toggle('hide')
+  })
+}
+
+if ($ttp) {
+  M.Tooltip.init($ttp)
+}
+
